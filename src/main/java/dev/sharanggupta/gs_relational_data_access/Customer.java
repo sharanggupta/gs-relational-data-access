30454987a888da1fbe958b3c3e450ff9dbf676a6
sharanggupta <sharang.gupta@sharanggupta.dev>
2024-11-16 11:56:47 +0000

    db operations added

--- src/main/java/dev/sharanggupta/gs_relational_data_access/Customer.java
+++ src/main/java/dev/sharanggupta/gs_relational_data_access/Customer.java
@@ -1,4 +1,45 @@
 package dev.sharanggupta.gs_relational_data_access;
 
 public class Customer {
+    private long id;
+    private String firstName, lastName;
+
+    public Customer(long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
--- src/main/java/dev/sharanggupta/gs_relational_data_access/GsRelationalDataAccessApplication.java
+++ src/main/java/dev/sharanggupta/gs_relational_data_access/GsRelationalDataAccessApplication.java
@@ -1,10 +1,51 @@
 package dev.sharanggupta.gs_relational_data_access;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
-public class GsRelationalDataAccessApplication {
+public class GsRelationalDataAccessApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(GsRelationalDataAccessApplication.class);
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Creating tables");
+
+		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+		jdbcTemplate.execute("CREATE TABLE customers(" +
+				"id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+		// Split up the array of whole names into an array of first/last names
+		List<Object[]> splitUpNames = Stream.of("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+				.map(name -> name.split(" "))
+				.collect(Collectors.toList());
+
+		// Use a Java 8 stream to print out each tuple of the list
+		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+		// Uses JdbcTemplate's batchUpdate operation to bulk load data
+		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+		log.info("Querying for customer records where first_name = 'Josh':");
+		jdbcTemplate.query(
+						"SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+						(rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")), "Josh")
+				.forEach(customer -> log.info(customer.toString()));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsRelationalDataAccessApplication.class, args);
