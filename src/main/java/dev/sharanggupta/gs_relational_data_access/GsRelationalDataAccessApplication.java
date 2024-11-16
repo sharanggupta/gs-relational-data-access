c8dd889e4d8f860d25636ee318ae4c2c5d12285e
sharanggupta <sharang.gupta@sharanggupta.dev>
2024-11-16 11:32:31 +0000

    Initial commit

---
+++ src/main/java/dev/sharanggupta/gs_relational_data_access/GsRelationalDataAccessApplication.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.gs_relational_data_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsRelationalDataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsRelationalDataAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sharanggupta/gs_relational_data_access/GsRelationalDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sharanggupta.gs_relational_data_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRelationalDataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
